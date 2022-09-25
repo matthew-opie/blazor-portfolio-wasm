78a09eceb7b2b2ea3b4452e17403c0a3e81a80d9
mopie <mattopie@gmail.com>
2022-09-25 07:47:00 -0400

    Add vibration to pomodoro alarm

--- blazor-portfolio-wasm/Program.cs
+++ blazor-portfolio-wasm/Program.cs
@@ -1,6 +1,7 @@
 // Copyright 2022 Matthew Opie (mattopie.com, matthewopie.com) All Rights Reserved.
 
 using blazor_portfolio_wasm;
+using CurrieTechnologies.Razor.Vibration;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddVibration();
 
 await builder.Build().RunAsync();
